e6691fef3c8b2bf6213afedaa537309dbabac7c4
Akash Rupareliya <47746940+Akash-Rupareliya@users.noreply.github.com>
2020-04-07 12:09:51 +0530

    web(code): added module 5

---
+++ Lab Exercises/Module 5/Module 5.sql
@@ -0,0 +1,14 @@
+use AdventureWorks
+
+CREATE TABLE Sales.MediaOutlet ( MediaOutletID INT NOT NULL, MediaOutletName NVARCHAR(40), PrimaryContact NVARCHAR (50), City NVARCHAR (50) ); 
+
+CREATE TABLE Sales.PrintMediaPlacement ( PrintMediaPlacementID INT NOT NULL, MediaOutletID INT, PlacementDate DATETIME, PublicationDate DATETIME, RelatedProductID INT, PlacementCost DECIMAL(18,2) );
+
+ALTER TABLE Sales.MediaOutlet ADD CONSTRAINT IX_MediaOutlet UNIQUE CLUSTERED ( MediaOutletID );
+
+CREATE UNIQUE CLUSTERED INDEX CIX_PrintMediaPlacement ON Sales.PrintMediaPlacement ( PrintMediaPlacementID ASC );
+
+CREATE NONCLUSTERED INDEX NCI_PrintMediaPlacement 
+ON [Sales].[PrintMediaPlacement] ([PublicationDate],[PlacementCost]) 
+INCLUDE ([PrintMediaPlacementID],[MediaOutletID],[PlacementDate],[RelatedProductID])
+
