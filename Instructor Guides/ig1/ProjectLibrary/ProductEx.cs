cb100e61a2b054226806438587b23dfea95b12f2
Akash Rupareliya <47746940+Akash-Rupareliya@users.noreply.github.com>
2020-05-01 23:12:48 +0530

    web(code): added instructor guides ig1 and ig2

---
+++ Instructor Guides/ig1/ProjectLibrary/ProductEx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectLibrary
+{
+    class ProductEx
+    {
+        string name;
+        double price;
+        ProductEx(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        public override string ToString()
+        {
+            string s = price.ToString();
+            return "Product: " + name + " " + s;
+        }
+
+    }
+}
